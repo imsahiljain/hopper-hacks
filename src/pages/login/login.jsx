6c76b73bbc6ab0224d66aa424890bd8be5c5bc57
imsahiljain <emailofsahiljain@gmail.com>
2022-02-12 23:35:48 +0530

    add: login form

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import LoginForm from "../../components/login-form/login-form";
+import Hero from "../../components/hero/hero";
+import Navbar from "../../components/navbar/navbar";
+
+const Login = () => {
+  return (
+    <>
+      <Navbar />
+      <LoginForm />
+    </>
+  );
+};
+
+export default Login;
