ab86c7e3d36b2b169af05105cc994fef0733812d
imsahiljain <emailofsahiljain@gmail.com>
2022-02-12 23:36:59 +0530

    add: firebase g-auth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home/home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../src/pages/home/home";
+import Login from "../src/pages/login/login";
+import Navbar from "./components/navbar/navbar";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { ToastContainer } from "react-toastify";
 
-function App() {
-  return (
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-    </Routes>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <>
+        <Navbar currentUser={this.state.currentUser} />
+        <ToastContainer />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </>
+    );
+  }
 }
-
 export default App;
