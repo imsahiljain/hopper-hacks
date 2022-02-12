bb071313e4da92b3249b20ad693e895646e893d7
imsahiljain <emailofsahiljain@gmail.com>
2022-02-12 23:36:43 +0530

    add: links on login

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,7 +1,27 @@
 import { Flex, Spacer, Box, Heading, Text, Button } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../../firebase/firebase.utils";
+import { useToast } from "@chakra-ui/react";
+import { IconButton } from "@chakra-ui/react";
+import { SettingsIcon } from "@chakra-ui/icons";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const toast = useToast();
+
+  const redirectToLoginPage = () => {
+    navigate("/login");
+  };
+  const logOut = () => {
+    auth.signOut();
+    toast({
+      title: "Logged Out.",
+      description: "Logged out successfully.",
+      status: "success",
+      duration: 3000,
+      isClosable: true,
+    });
+  };
   return (
     <>
       <Flex
@@ -42,17 +62,45 @@ const Navbar = () => {
             <Link to="/">About NFT</Link>
           </Button>
 
-          <Button
-            colorScheme="gray"
-            borderColor="gray.500"
-            p="5"
-            variant="outline"
-            mr="30"
-            fontFamily="Neue-Regular"
-            fontSize="18px"
-          >
-            Login
-          </Button>
+          {auth.currentUser ? (
+            <>
+              <Button
+                colorScheme="gray"
+                variant="ghost"
+                mr="3"
+                fontFamily="Neue-Regular"
+                fontSize="18px"
+              >
+                <Link to="/">Martketplace</Link>
+              </Button>
+              <Button
+                colorScheme="gray"
+                borderColor="gray.500"
+                p="5"
+                variant="outline"
+                mr="30"
+                fontFamily="Neue-Regular"
+                fontSize="18px"
+                onClick={logOut}
+              >
+                <Link to="/login">Logout</Link>
+              </Button>
+              <IconButton aria-label="Your Account" icon={<SettingsIcon />} />
+            </>
+          ) : (
+            <Button
+              colorScheme="gray"
+              borderColor="gray.500"
+              p="5"
+              variant="outline"
+              mr="30"
+              fontFamily="Neue-Regular"
+              fontSize="18px"
+              onClick={redirectToLoginPage}
+            >
+              <Link to="/login">Login</Link>
+            </Button>
+          )}
         </Box>
       </Flex>
     </>
